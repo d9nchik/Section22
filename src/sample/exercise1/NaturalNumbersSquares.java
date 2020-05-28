5cac8f45940106e269432145e43e72043d1d1ae1
Danylo Halaiko <dan.galaiko@gmail.com>
2020-05-28 13:29:14 +0300

    Exercise1 has been done. Natural numbers and their squares.

--- src/sample/exercise1/NaturalNumbersSquares.java
+++ src/sample/exercise1/NaturalNumbersSquares.java
@@ -2,7 +2,8 @@ package sample.exercise1;
 
 public class NaturalNumbersSquares {
     public static void main(String[] args) {
-        showSequenceSquare(10);
+        showSequenceSquare(15);
+        showSequenceCube(15);
     }
 
     private static void showSequenceSquare(int number) {
@@ -10,6 +11,16 @@ public class NaturalNumbersSquares {
         for (int i = 0; i <= number; i++) {
             previousSquare += (i << 1) - 1;
             System.out.println(i + "^2 = " + previousSquare);
+        }
+    }
+
+    private static void showSequenceCube(int number) {
+        int previousSquare = 1;
+        int previousCube = -1;
+        for (int i = 0; i <= number; i++) {
+            previousCube += previousSquare * 3 + i * 3 - 2;
+            previousSquare += (i << 1) - 1;
+            System.out.println(i + "^3 = " + previousCube);
         }
     }
 }
