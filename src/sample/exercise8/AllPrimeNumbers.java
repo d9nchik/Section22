7f4e076a39de7839f6207cc6f21e4019436e54ba
Danylo Halaiko <dan.galaiko@gmail.com>
2020-05-30 09:26:36 +0300

    Exercise8 has been done. A program that finds all prime numbers up to 10,000,000,000. There are approximately 455,052,511 such prime numbers. I started batch job, when task will be finished I will upload the result.

---
+++ src/sample/exercise8/AllPrimeNumbers.java
@@ -0,0 +1,122 @@
+package sample.exercise8;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
+
+public class AllPrimeNumbers {
+    private static final File FILE = new File("primeNumbers.dat");
+    private static final int SIZE_OF_READ_BLOCK = 10_000;
+    private static final long MAX_NUMBER = 10_000_000_000L;
+    private static final int SIZE_OF_WRITE_BLOCK = 1_000;
+
+    public static void main(String[] args) {
+
+        // A list to hold prime numbers
+        long[] array = new long[SIZE_OF_WRITE_BLOCK];
+
+        int count = 0; // Count the number of prime numbers
+        long number = startNumber() + 1; // A number to be tested for primeness
+        long squareRoot = (long) Math.sqrt(number); // Check whether number <= squareRoot
+        int position = 0;
+
+        // Repeatedly find prime numbers
+        while (number <= MAX_NUMBER) {
+            // Assume the number is prime
+
+
+            if (squareRoot * squareRoot < number) squareRoot++;
+
+            boolean isPrime = isPrime(number, squareRoot);// Is the current number prime?
+
+
+            // Test whether number is prime
+            for (int k = 0; isPrime && k < position
+                    && array[k] <= squareRoot; k++) {
+                if (number % array[k] == 0) { // If true, not prime
+                    isPrime = false; // Set isPrime to false
+                    break; // Exit the for loop
+                }
+            }
+
+            // Print the prime number and increase the count
+            if (isPrime) {
+                array[position++] = number;// Add a new prime to the list
+                count++; // Increase the count
+                if (count % SIZE_OF_WRITE_BLOCK == 0) {
+                    // Print the number and advance to the new line
+                    writeNumbers(array);
+                    position = 0;
+                }
+            }
+
+            // Check whether the next number is prime
+            number++;
+            if (number % 10_000 == 0)
+                System.out.println(number);
+        }
+
+        writeNumbers(Arrays.copyOfRange(array, 0, position));
+        System.out.println("\n" + count +
+                " prime(s) less than or equal to " + MAX_NUMBER);
+    }
+
+    private static boolean isPrime(long number, long squareRoot) {
+        long[] primeNumbers = numbers(0);
+        int positionInFile = 0;
+        int primeNumberPosition = 0;
+        while (primeNumbers.length != 0 && primeNumbers[primeNumberPosition] <= squareRoot) {
+            if (number % primeNumbers[primeNumberPosition] == 0) {
+                return false;
+            }
+            primeNumberPosition++;
+            if (primeNumberPosition == primeNumbers.length) {
+                positionInFile += SIZE_OF_READ_BLOCK;
+                primeNumbers = numbers(positionInFile);
+                primeNumberPosition = 0;
+            }
+        }
+        return true;
+    }
+
+    private static long[] numbers(int pos) {
+        try (RandomAccessFile input = new RandomAccessFile(FILE, "r")) {
+            input.seek(pos);
+            long size = Math.min(SIZE_OF_READ_BLOCK, input.length() - pos);
+            long[] array = new long[SIZE_OF_READ_BLOCK];
+            for (int i = 0; i < size; i++) {
+                array[i] = input.readLong();
+            }
+            return array;
+        } catch (IOException ex) {
+            return new long[0];
+        }//System.out.println("Problem with stream!");
+
+    }
+
+    private static long startNumber() {
+        try (RandomAccessFile input = new RandomAccessFile(FILE, "r")) {
+            input.seek(input.length() - 8);
+            return input.readLong();
+        } catch (FileNotFoundException ex) {
+            return 1;
+        } catch (IOException ex) {
+            System.out.println("Problem with stream!");
+            return 1;
+        }
+    }
+
+    private static void writeNumbers(long[] numbers) {
+        try (RandomAccessFile output = new RandomAccessFile(FILE, "rw")) {
+            output.seek(output.length());
+            for (long number : numbers)
+                output.writeLong(number);
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found!");
+        } catch (IOException ex) {
+            System.out.println("Problem with stream!");
+        }
+    }
+}
